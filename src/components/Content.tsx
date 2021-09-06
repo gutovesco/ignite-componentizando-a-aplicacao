ea0844c3b669beda03ef33b3d1b8ac9fbd4097bb
gutovesco <guto.vesco@hotmail.com>
2021-09-06 15:06:15 -0300

    challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
-export function Content() {
-  // Complete aqui
+import { Button } from "./Button";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  movies: any;
+  selectedGenre: any;
 }
+
+export const Content = ({ movies, selectedGenre }: ContentProps) => {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie: any) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
